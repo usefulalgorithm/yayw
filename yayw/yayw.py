739ee1a6a818b8124e14a655811f06b019744b81
Tsung-Ju Lii <usefulalgorithm@gmail.com>
2025-02-10 17:48:39 +0800

    feat: better splitting mechanism

--- yayw/yayw.py
+++ yayw/yayw.py
@@ -1,20 +1,21 @@
 import os
-import sys
-from typing import List, Optional
+from typing import Optional
 
 from httpx import post
 from loguru import logger
 
+from yayw.splitter import Splitter
+
 
 class YAYW:
     def __init__(
-        self, user_id: str, access_token: str, chunk_threshold: int = 450
+        self, user_id: str, access_token: str, chunk_threshold: int = 500
     ) -> None:
         self._base_url = "https://graph.threads.net/v1.0"
         self._user_id = user_id
         # TODO think refresh mechanism
         self._access_token = access_token
-        self._chunk_threshold = chunk_threshold
+        self._max_characters_per_post = chunk_threshold
 
     def _create_and_publish_post(self, text: str, reply_to_id: Optional[str]) -> str:
         resp = post(
@@ -41,41 +42,19 @@ class YAYW:
         media_id = resp.json()["id"]
         return media_id
 
-    def _split_lines_into_chunks(self, lines: List[str]) -> List[str]:
-        texts: List[str] = []
-        chunk, char_count = [], 0
-        for line in lines:
-            line_char_count = len(line)
-
-            if char_count + line_char_count > self._chunk_threshold:
-                # finalize chunk and make new one
-                texts.append("".join(chunk))
-                chunk, char_count = [], 0
-
-            chunk.append(line)
-            char_count += line_char_count
-
-        if chunk:
-            texts.append("".join(chunk))
-
-        logger.info(f"# of chunks: {len(texts)}")
-
-        # Finalize chunks
-        texts = [text + f" ({i + 1}/{len(texts)})" for i, text in enumerate(texts)]
-
-        return texts
-
     def run(self, path: str) -> None:
         with open(path) as f:
-            texts = self._split_lines_into_chunks(f.readlines())
+            posts = Splitter(
+                max_characters_per_post=self._max_characters_per_post
+            ).split_into_posts(f.read())
 
-        if not texts:
+        if not posts:
             logger.warning("Nothing to yap about")
             return
 
         media_id = None
-        for text in texts:
-            media_id = self._create_and_publish_post(text, reply_to_id=media_id)
+        for current_post in posts:
+            media_id = self._create_and_publish_post(current_post, reply_to_id=media_id)
             logger.info(f"Created post: {media_id}")
 
 
@@ -83,6 +62,6 @@ def yayw(
     path: str,
     user_id: str = os.environ["YAYW_USER_ID"],
     access_token: str = os.environ["YAYW_ACCESS_TOKEN"],
-    max_post_length: int = int(os.environ.get("YAYW_MAX_POST_LENGTH", 450)),
+    max_post_length: int = int(os.environ.get("YAYW_MAX_POST_LENGTH", 500)),
 ):
     YAYW(user_id, access_token, max_post_length).run(path)
